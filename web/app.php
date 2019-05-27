ae4f79b2b489505d58f6aa8fbe2b784384048ec8
Benjamin Bennett <synaptology@gmail.com>
2019-05-27 15:48:15 +0100

    Removing loadClassCache() method call.

--- web/app.php
+++ web/app.php
@@ -10,8 +10,6 @@ include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 Request::setTrustedProxies(['192.0.0.1', '10.0.0.0/8'], Request::HEADER_X_FORWARDED_ALL);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
